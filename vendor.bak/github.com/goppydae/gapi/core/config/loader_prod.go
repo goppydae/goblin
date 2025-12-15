c4bc5fcde0b429cd6a615ce35a6bd8af4c669d9b
enqack <enqack@gmail.com>
2025-12-15 03:51:53 -0500

    Clean-up.

--- vendor.bak/github.com/goppydae/gapi/core/config/loader_prod.go
+++
@@ -1,9 +0,0 @@
-//go:build !dev
-
-package config
-
-import "github.com/spf13/viper"
-
-func addDefaultPaths() {
-	viper.AddConfigPath("/etc/gapi")
-}
--- vendor.bak/github.com/goppydae/gapi/internal/eventbus/interface.go
+++
@@ -1,8 +0,0 @@
-package eventbus
-
-type Transport[T any] interface {
-	PublishRemote(Event[T]) error
-	Broadcast(Event[T]) error
-	OnRemoteEvent(func(Event[T]))
-	Close() error
-}
